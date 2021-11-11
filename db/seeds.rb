4d0ecb094999a4323feff74d62d97958cf826003
Perrine Torcol <perrine.trcl@gmail.com>
2021-11-11 14:23:57 +0100

    DB seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=9e9560886d098532d4aeade83ab91ad4&language=en-US&page=1'
+parsed_movies = URI.open(url).read
+movies_results = JSON.parse(parsed_movies)
+movies = movies_results['results']
+
+movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts "DB seeded DUDETTE 🌱"
